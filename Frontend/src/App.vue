a175c0f5f875bd6982c3b6926dcf2111596f9b9e
SifisoDuba <223072559@mycput.ac.za>
2025-06-23 19:03:37 +0200

    Commiting for the last time, it rejected 7 times

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,35 +1,18 @@
 <template>
   <div id="app">
-    <h1>Vue + Express Full Stack App</h1>
-  <p> I didn't copy this code i created it from scratch, this is to prove it is not a tamplate <br>(🕊️in loving memory of Sifiso Duba 2003-2025🕊️)</p>
-    <p>{{ message }}</p>
+    <Dashboard />
+    <router-view /> 
   </div>
 </template>
 
 <script>
+import Dashboard from './pages/Dashboard.vue';
+
 export default {
-  data() {
-    return {
-      message: 'Loading...'
-    };
+  name: 'App',
+  components: {
+    Dashboard,
   },
-  mounted() {
-    fetch('/api/message')
-      .then(res => res.json())
-      .then(data => {
-        this.message = data.message;
-      })
-      .catch(err => {
-        console.error(err);
-        this.message = 'Error connecting to backend.';
-      });
-  }
 };
 </script>
 
-<style>
-body {
-  font-family: Arial, sans-serif;
-  text-align: center;
-}
-</style>
---
+++ Frontend/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="footer">
+    <div class="footer-content">
+      <div class="footer-links">
+        <router-link to="/">Dashboard</router-link>
+        <router-link to="/report">Report Crime</router-link>
+        <router-link to="/about">Crimes</router-link>
+        <router-link to="/contact">Emergency contacts</router-link>
+        <router-link to="/contact">About us</router-link>
+      </div>
+      <div class="footer-info">
+        <p>©{{ new Date().getFullYear() }} Crime Report System. All rights reserved.</p>
+        <p class="disclaimer">⚠️ Please note: False reports may result in legal action.</p><br>
+        <p><router-link to="https://www.saps.gov.za/legislation/acts/act68of1995.pdf">Terms & Conditions</router-link> | <router-link to="/privacy">Privacy Policy</router-link></p>
+        
+      </div>
+      <div class="Emergency-contacts">
+        <p>Emergency Contacts:</p>
+        <p>Police: 10111</p>
+        <p>Ambulance: 10177</p>
+        <p>Fire Department: 10177</p>
+        <p>Crime Stop: 086 001 0111</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+};
+</script>
+
+<style src="@/assets/styles/Footer.css"></style>
