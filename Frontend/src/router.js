8d90b315e91a6b9d83aa12a4e76581580e1ae226
SifisoDuba <223072559@mycput.ac.za>
2025-07-12 01:08:40 +0200

    Report summary page and with components

--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -3,12 +3,14 @@ import AdminLogin from './pages/AdminLogin.vue'
 import Dashboard from './pages/Dashboard.vue'
 import Landing from './pages/Landing.vue'
 import ReportingPage from './pages/ReportingPage.vue'
+import ReportSummary from './pages/ReportSummary.vue'
 
 const routes = [
     {path : '/', component: Landing},
     {path : '/admin-login', component: AdminLogin},
     {path : '/dashboard', component: Dashboard},
     {path : '/reportingpage', component: ReportingPage},
+    {path : '/reportsummary', component: ReportSummary}
 ]
 const router = createRouter({
     history: createWebHistory(),
